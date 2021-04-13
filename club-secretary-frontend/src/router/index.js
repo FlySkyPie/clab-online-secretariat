0cefce982b52cd4584ea59c97e24b321b60f8880
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2021-04-14 07:59:24 +0800

    Add redirect to frontend.

--- club-secretary-frontend/src/router/index.js
+++ club-secretary-frontend/src/router/index.js
@@ -1,5 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import jwt_decode from "jwt-decode";
+import Cookies from 'js-cookie';
+
+const getCookiePromise = () => new Promise((resolve, reject) => {
+  const jwt = Cookies.get('jwt');
+  if (jwt === undefined) {
+    reject();
+    return;
+  }
+  resolve(jwt)
+});
+
+const decodePromese = (tokenString) => new Promise((resolve, reject) => {
+  try {
+    const decode = jwt_decode(tokenString);
+    resolve(decode);
+  } catch (error) {
+    reject();
+  }
+});
 
 Vue.use(VueRouter)
 
@@ -18,5 +38,17 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach(async (to, from, next) => {
+  const token = await getCookiePromise();
+  const jwt = await decodePromese(token);
+  if (to.name === jwt.type) {
+    next();
+  }else if (jwt.type === 'member-contacts') {
+    next({ name: 'member-contacts' });
+  }else{
+    next({ name: 'homepage' });
+  }/***/
+});
 
 export default router
