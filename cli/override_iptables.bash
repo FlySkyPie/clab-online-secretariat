ad220099de9a802bff3d81a0775afe6b2c0fde35
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2021-05-10 07:10:45 +0800

    Complete the script.

--- cli/override_iptables.bash
+++ cli/override_iptables.bash
@@ -5,6 +5,7 @@
 #date            :2021-05-09
 #version         :0.1.0
 #usage           :bash ./cli/override_iptables.bash $(curl ifconfig.me 2>/dev/null)
+#requirement     :This script would modify iptables, make sure executor have permission.
 #notes           :--
 #bash_version    :4.4.20(1)-release
 #===============================================================================
@@ -24,15 +25,26 @@ if [ -z "${parameter}" ]; then
 fi
 echo "Using: ${parameter}"
 
+# Back up original Masquerade.
 TABLE=$(iptables -S -t nat |
     grep "^-A POSTROUTING -s [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*.*-o .*" |
     sed -r -e "s/-A POSTROUTING -s ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/[0-9]+).*-o (.+) -j MASQUERADE/\1,\2/")
 
+# Remove rules.
+while true; do
+    RULE_NUMBER=iptables -t nat -v -L POSTROUTING -n --line-number | grep "0.0.0.0/0.*$" | sed -re "s/^([0-9]+).*/\1/" | head -1
+    if [ -z "${RULE_NUMBER}" ]; then
+        break
+    else
+        iptables -t nat -D PREROUTING ${RULE_NUMBER}
+    fi
+done
+
+# Add rules.
 for row in ${TABLE}; do
-    IP=$(echo ${row} |  sed -r -e "s/(.*),.*/\1/")
-    NAME=$(echo ${row} |   sed -r -e "s/.*,(.*)/\1/" )
-    echo "address: ${IP}"
-    echo "name: ${NAME}"
+    IP=$(echo ${row} | sed -r -e "s/(.*),.*/\1/")
+    NAME=$(echo ${row} | sed -r -e "s/.*,(.*)/\1/")
+    iptables -t nat -A POSTROUTING -s ${IP} ! -o ${NAME} -j SNAT --to-source ${parameter}
 done
 
 exit 0
